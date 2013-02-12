295dcb0bc56af69a2748af21a53111596175bc9c
vincent.lark@gmail.com <vincent.lark@gmail.com>
2013-02-12 14:49:23 +0100

    use crappy hack to pass room options

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@
 exports.index = function(req, res){
   res.render('index', {
 		title: 'Twitter says',
-		roomOptions: {
+		roomOptions: JSON.stringify({
 			name: JSON.stringify(req.params),
 
 			tag: req.param('tag', false),
 			lang: req.param('lang', false),
 			country: req.param('country', false),
-		},
+		}),
 
 		locations: require('locations').countries,
 		
