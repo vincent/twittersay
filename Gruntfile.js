416907fd04c022e5c0825c6261f20e214a048af1
vincent.lark@gmail.com <vincent.lark@gmail.com>
2013-02-19 23:15:48 +0100

    grunt 0.4 god dammit + update deps

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    shell: {
+      // Generate documentation
+      makeDocs: {
+        command: 'ls && doxx --source . --ignore "public,static,views,templates,node_modules,grunt,config" --target public/docs'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true,
+        strict: false
+      },
+      globals: {
+        jQuery: true
+      },
+      uses_defaults: [ '*.js', 'lib/**/*.js', 'test/**/*.js' ]
+    } 
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+
+};
